451fcbcf10aa1c8d4f381e61a253fd8f2eec2123
Onur <onur.cagirici@gmail.com>
2015-12-13 16:36:53 +0200

    Toshibro

--- Main.py
+++ Main.py
@@ -1,17 +1,20 @@
 import Sensor
+import graph
 import random
+import plotter
 
 sensors = []
 
-for i in range(1, 10):
-    x = random.uniform(0.0, 10.0)
-    y = random.uniform(0.0, 10.0)
-    z = random.uniform(0.0, 10.0)
+for i in range(5):
+    x = random.uniform(0.0, 1.0)
+    y = random.uniform(0.0, 1.0)
+    z = random.uniform(0.0, 1.0)
     sensors.append(Sensor.Sensor(i, x, y, z))
 
+g = graph.WSN(sensors)
+print(g)
+plotter.plot_actual(g)
 
-for sensor in sensors:
-    print(sensor)
 
 
 
--- Sensor.py
+++ Sensor.py
@@ -3,24 +3,25 @@ from Goulib.geom import Point3, Point2
 
 class Sensor:
 
-    id = 0
-    actualPos = Point3(0,0,0)
-    localPos = Point2(0,0)
-    globalPos = Point3(0,0,0)
-    neighbors = []
+    actualPos = Point3(0, 0, 0)
+    localPos = Point2(0, 0)
+    globalPos = Point3(0, 0, 0)
+    neighbors = {}
 
-    def __init__(self, id, x,y,z):
-        self.id = id
-        self.actualPos = Point3(x,y,z)
+    def __init__(self, node, x, y, z):
+        self.id = node
+        self.actualPos = Point3(x, y, z)
+        print('Sensor', id, 'created')
 
-    def actualPos(self):
+    def actual_pos(self):
         return [self.actualPos.x(), self.actualPos.y(), self.actualPos.z()]
 
-    def actualDistance(self, other):
+    def actual_distance(self, other):
         return self.actualPos.distance(other.actualPos)
 
-    def addNeighbor(self, neighbor):
-        self.neighbors.append(neighbor)
+    def add_neighbor(self, neighbor, distance):
+        self.neighbors[neighbor] = distance
+        print(self.id, ':', self.neighbors)
 
     def __repr__(self):
         instr = "{0}: ({1:.2f}, {2:.2f}, {3:.2f})".format(self.id, self.actualPos.x, self.actualPos.y, self.actualPos.z)
