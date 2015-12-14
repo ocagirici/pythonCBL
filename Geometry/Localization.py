1006184ae498a296629b6f4fac979dfa3cea4147
Onur <onur.cagirici@gmail.com>
2015-12-14 20:37:32 +0200

    Toshibro

---
+++ Geometry/Localization.py
@@ -0,0 +1,6 @@
+V = 10
+for a in range(V-3):
+        for b in range(a+1, V-2):
+            for c in range(b+1, V-1):
+                for d in range(c+1, V):
+                    print(a, b, c, d)
--- Sensor.py
+++ Sensor.py
@@ -1,64 +1,44 @@
-from Goulib.geom import Point3, Point2, Sphere
-import numpy
-import sympy
+import Geometry.vector as point
+
 
 class Sensor:
-
-    actualPos = Point3(0, 0, 0)
-    localPos = Point2(0, 0)
-    globalPos = Point3(0, 0, 0)
-    neighbors = {}
-    localized_neighbors = []
+    actualPos = []
+    localPos = [0, 0]
+    globalPos = []
+    localized_neighbors_3d = []
+    localized_neighbors_2d = []
+    localized_2d = False
+    localized_3d = False
 
     def __init__(self, node, x, y, z):
         self.id = node
-        self.actualPos = Point3(x, y, z)
-        print('Sensor', id, 'created')
-
-    def actual_pos(self):
-        return [self.actualPos.x(), self.actualPos.y(), self.actualPos.z()]
+        self.actualPos = point.vector([x, y, z])
 
     def actual_distance(self, other):
-        return self.actualPos.distance(other.actualPos)
+        return point.norm(self.actualPos - other.actualPos)
 
-    def add_neighbor(self, neighbor, distance):
-        self.neighbors[neighbor] = distance
+    def neighbor_localized_2d(self, neighbor):
+        if self.localized_2d:
+            return
+        self.localized_neighbors_2d.append(neighbor)
 
-    def neighbor_localized(self, neighbor):
-        self.localized_neighbors.append(neighbor)
+    def neighbor_localized_3d(self, neighbor):
+        if self.localized_3d:
+            return
+        self.localized_neighbors_3d.append(neighbor)
 
-    def dist(self, neighbor):
-        return self.neighbors[neighbor]
+    def set_local(self, pos):
+        self.localPos = pos
+        self.localized_2d = True
 
-    def localize_self_3d(self):
-        if len(self.localized_neighbors) <= 4:
-            return False
-        i = self.neighbors[self.localized_neighbors[0]].localPos
-        j = self.neighbors[self.localized_neighbors[1]].localPos
-        k = self.neighbors[self.localized_neighbors[2]].localPos
-        l = self.neighbors[self.localized_neighbors[3]].localPos
-        if self.coplanar(i, j, k, l):
-            return False
-        sympy.
+    def set_global(self, pos):
+        self.globalPos = pos
+        self.localized_3d = True
 
-    def coplanar(self, *points):
-        i = sympy.Point3D(points[0].x, points[0].y, points[0].z)
-        j = sympy.Point3D(points[1].x, points[1].y, points[1].z)
-        k = sympy.Point3D(points[2].x, points[2].y, points[2].z)
-        l = sympy.Point3D(points[3].x, points[3].y, points[3].z)
-        return sympy.Point.is_collinear(i, j, k, l)
-
-
-
-
+    def seed(self):
+        self.set_global(self.actualPos)
 
     def __repr__(self):
-        instr = "{0}: ({1:.2f}, {2:.2f}, {3:.2f})".format(self.id, self.actualPos.x, self.actualPos.y, self.actualPos.z)
+        instr = "{0}: ({1:.2f}, {2:.2f}, {3:.2f})".format(self.id,
+                                                          self.actualPos[0], self.actualPos[1], self.actualPos[2])
         return instr
-
-
-
-
-
-
-
